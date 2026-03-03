c109d0b79b1110e75e0a8b8835cc29944f790a96
owl1753 <bighammer1753@gmail.com>
2026-03-03 09:41:50 +0900

    10871

--- p10871/src/main.rs
+++ p10871/src/main.rs
@@ -1,3 +1,24 @@
+use std::io::{stdin, stdout, BufWriter, Write};
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+
+    stdin().read_line(&mut input).unwrap();
+    let mut input = input.split_ascii_whitespace().flat_map(str::parse::<i32>);
+
+    let n = input.next().unwrap();
+    let x = input.next().unwrap();
+
+    let mut input = String::new();
+    stdin().read_line(&mut input).unwrap();
+    let mut input = input.split_ascii_whitespace().flat_map(str::parse::<i32>);
+    let stdout = stdout();
+    let mut writer = BufWriter::new(stdout);
+
+    for i in 0..n {
+        let el = input.next().unwrap();
+        if el < x {
+            write!(writer, "{} ", el).unwrap();
+        }
+    }
 }
